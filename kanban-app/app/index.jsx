cf43f7df00c39d0b8b124018639bfb9c0295414f
zhaqiang <hugochougt@gmail.com>
2017-03-14 23:01:37 +0800

    Kanban app of 'SurviveJS - React'

---
+++ kanban-app/app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+import Provider from './components/Provider';
+
+if(process.env.NODE_ENV !== 'production') {
+  React.Perf = require('react-addons-perf');
+}
+
+ReactDOM.render(
+  <Provider><App /></Provider>,
+  document.getElementById('app')
+);
